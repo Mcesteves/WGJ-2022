567d8457ffddec6b62dec81f220ee131d8271290
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2022-08-21 22:24:50 -0300

    Começa Menu de Pause

---
+++ WGJ_2022/Assets/Scripts/UI/InGameMenuManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InGameMenuManager : MonoBehaviour
+{
+    public GameObject SettingsUI;
+    public GameObject PauseUI;
+    public GameObject PauseCanvas;
+    void Start()
+    {
+        
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        PauseCanvas.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        PauseCanvas.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        PauseUI.SetActive(false);
+        SettingsUI.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        SettingsUI.SetActive(false);
+        PauseUI.SetActive(true);
+        
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1.0f;
+    }
+}
